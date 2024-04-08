dcc3666548d7656e34b754e3579b7734fef838f7
Andrew Fong <fong.a@saintaug.nsw.edu.au>
2024-04-09 09:18:39 +1000

    first commit

---
+++ Development Files/hello_world.py
@@ -0,0 +1,8 @@
+# 1 - install GIT locally
+# 2 - set up your GIT email and username using the GIT commands
+    # Set your username: git config --global user.name "FIRST_NAME LAST_NAME"
+    # Set your email address: git config --global user.email "MY_NAME@example.com"
+# 3 - Create a GitHub account, nice profile pic 😉, use school email
+# 4 - Login to GitHub within VSCode (button with head on bottom left)
+# 5 - Setup a development folder somewhere on your computer
+# 6 - Open as folder within VSCode File > Open Folder > Major Folder 
